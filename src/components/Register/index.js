a598046b7f8fae64f4a270ab56c0ac848fb2d279
bjtlr0 <bjtlr@naver.com>
2020-05-08 11:20:37 +0900

    add Register Header component export

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -2,3 +2,4 @@ export {default as PersonalInput} from './PersonalInput';
 export {default as ExperienceInput} from './ExperienceInput';
 export {default as DevelopInput} from './DevelopInput';
 export {default as CompleteRegister} from './CompleteRegister';
+export {default as RegisterHeader} from './RegisterHeader';
